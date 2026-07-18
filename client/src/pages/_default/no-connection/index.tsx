dc929272324f1c8984fc55b57f7e76dbf245c8dd
Anon <Anon>
2026-07-18 02:05:23 -0400

    Align offline message with page content

--- client/src/pages/_default/no-connection/index.tsx
+++ client/src/pages/_default/no-connection/index.tsx
@@ -1,12 +1,15 @@
+import Page from '~components/root/page';
 import Section from '~components/root/section';
 
 export default function NoConnection() {
 	return (
-		<Section heading='Server Connection Offline'>
-			<p>
-				The Web UI cannot connect to the server. Check that the server is running and
-				reachable. It will reconnect automatically.
-			</p>
-		</Section>
+		<Page heading='Server Connection Offline'>
+			<Section>
+				<p>
+					The Web UI cannot connect to the server. Check that the server is running and
+					reachable. It will reconnect automatically.
+				</p>
+			</Section>
+		</Page>
 	);
 }
